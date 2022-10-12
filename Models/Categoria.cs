8902df608cb447861159c25a8085f8b61e01c0bc
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-11 21:24:44 -0500

    Se realiza migracion para crear la BD y la tabla del modelo categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,7 +4,9 @@ namespace PeliculaAPI.Models {
     public class Categoria {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public bool Estado { get; set; }
     }
 }
