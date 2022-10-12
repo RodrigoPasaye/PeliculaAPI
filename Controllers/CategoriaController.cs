b8f4ca20b41baa6e2e583f2493f0e39eaa54ecf0
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-11 21:51:08 -0500

    Se agrega el controlador de Categorias y los metodos de get y post
    ** Se agrega metodo que obtiene todas las categorias
    ** Se agrega metodo que obtiene la categoria por id
    ** Se agrega metodo para crear una nueva categoria

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculaAPI.Data;
+using PeliculaAPI.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculaAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase {
+        private readonly AplicationDbContext _context;
+
+        public CategoriaController(AplicationDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategorias() {
+            var categorias = await _context.Categorias.OrderBy(categoria => categoria.Nombre).ToListAsync();
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCategoria(int id) {
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(categoria => categoria.Id == id);
+            if (categoria == null) {
+                return NotFound();
+            }
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearCategoria([FromBody] Categoria categoria) {
+            if (categoria == null) {
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            await _context.AddAsync(categoria);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
