d7ca1dd9d4591f8f4a1ba467acd56aea60bdb745
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-11 20:56:48 -0500

    Se agrega el modelo de Categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculaAPI.Models {
+    public class Categoria {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Estado { get; set; }
+    }
+}
