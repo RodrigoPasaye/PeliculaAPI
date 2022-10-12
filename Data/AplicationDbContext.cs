aeecdfa7faacf0dc674f2f509256cb4cd11c2171
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-11 20:59:16 -0500

    Se agrega la clase del DbContext y se instala el paquete de EntityFrameworkCore

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculaAPI.Models;
+
+namespace PeliculaAPI.Data {
+    public class AplicationDbContext : DbContext {
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
