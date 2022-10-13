2b01180ce257920fa67162d5072fe8fb387f3f6e
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-12 19:40:10 -0500

    Se agrega el modelo de peliculas
    ** Se agrega al Db Context y se realiza la migracion para crear la tabla en la BD

--- Migrations/20221013003538_AgregarPeliculaDB.Designer.cs
+++ Migrations/20221013003538_AgregarPeliculaDB.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PeliculaAPI.Data;
 
 namespace PeliculaAPI.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221013003538_AgregarPeliculaDB")]
+    partial class AgregarPeliculaDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeliculaAPI.Models {
+    public class Pelicula {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NombrePelicula { get; set; }
+        [Required]
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+    }
+}
