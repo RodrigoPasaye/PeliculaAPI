99cdf1016531b27e2bc3e10a61faa8578e2807a8
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-12 21:12:36 -0500

    Se agrega los codigos de estado que devuelve el servicio
    ** Se agregan comentarios a los end points

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PeliculaAPI.Data;
 using PeliculaAPI.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,13 +17,27 @@ namespace PeliculaAPI.Controllers {
             _context = context;
         }
 
+        /// <summary>
+        /// Obtiene una lista con todas las Categorias registradas
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Categoria>))]
+        [ProducesResponseType(400)] // Bad Request
         public async Task<IActionResult> GetCategorias() {
             var categorias = await _context.Categorias.OrderBy(categoria => categoria.Nombre).ToListAsync();
             return Ok(categorias);
         }
 
-        [HttpGet("{id:int}")]
+        /// <summary>
+        /// Obtiene una Categoria filtrada por su id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id:int}", Name = "GetCategoria")]
+        [ProducesResponseType(200, Type = typeof(Categoria))]
+        [ProducesResponseType(400)] // Bad Request
+        [ProducesResponseType(404)] // Not Found
         public async Task<IActionResult> GetCategoria(int id) {
             var categoria = await _context.Categorias.FirstOrDefaultAsync(categoria => categoria.Id == id);
             if (categoria == null) {
@@ -31,7 +46,15 @@ namespace PeliculaAPI.Controllers {
             return Ok(categoria);
         }
 
+        /// <summary>
+        /// Crea una nueva Categoria
+        /// </summary>
+        /// <param name="categoria"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)] // Bad Request
+        [ProducesResponseType(500)] // Error Interno
         public async Task<IActionResult> CrearCategoria([FromBody] Categoria categoria) {
             if (categoria == null) {
                 return BadRequest(ModelState);
@@ -42,7 +65,7 @@ namespace PeliculaAPI.Controllers {
 
             await _context.AddAsync(categoria);
             await _context.SaveChangesAsync();
-            return Ok();
+            return CreatedAtRoute("GetCategoria", new { id = categoria.Id }, categoria);
         }
     }
 }
